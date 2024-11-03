fac1cc922d1538ebeecc82c368c1325ae67df47d
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2024-11-03 18:34:38 +0700

    Update scripttui.js

--- scripttui.js
+++ scripttui.js
@@ -41,7 +41,7 @@ async function fetchData() {
         <td>${item.maxcoin}</td>
         <td class="countdown" data-start-time="${item.startTime}"></td>
         <td>
-          <button onclick="window.location.href='https://shopee.vn/universal-link/shop/${item.shopId}'">
+          <button onclick="window.location.href='https://shopee.vn/universal-link/shop/${item.shopId}?utm_source=an_17333510032&utm_medium=affiliates&utm_campaign=-&utm_content=acc48----&utm_term=c1id8gpxye2t'">
             Đến Shop
           </button>
         </td>
