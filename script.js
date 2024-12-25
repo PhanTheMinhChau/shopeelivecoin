7f29146a6be478f18d544a90e1bb00d1d22fba2e
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2024-12-25 15:00:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ async function fetchData() {
           <div class="shop-name"> ${item.userName}</div>
           <div class="coin-section">${item.maxcoin} xu</div>
           <div class="button-section">
-            <button onclick="window.location.href='https://shopee.vn/universal-link/shop/${item.shopId}?utm_source=an_17358480143&utm_medium=affiliates&utm_campaign=-&utm_content=acc48----&utm_term=caii1qpv7591;'">Vào ngay</button>
+            <button onclick="window.location.href='https://shopee.vn/universal-link/shop/${item.shopId}?utm_source=an_17396220028&utm_medium=affiliates&utm_campaign=-&utm_content=acc48----&utm_term=can4k167maqd;'">Vào ngay</button>
           </div>
         </div>
         <div class="countdown" data-start-time="${item.startTime}"></div>
