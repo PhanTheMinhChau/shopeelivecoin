e3e9265164038ad29e5905e24b6986036ad1e2ec
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2025-04-13 00:03:35 +0700

    Update script.js

--- cayxu/script.js
+++ cayxu/script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = document.getElementById('password').value;
         
         try {
-            const response = await fetch('http://localhost:8000/login', {
+            const response = await fetch('https://shopee-api-7q9q.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
